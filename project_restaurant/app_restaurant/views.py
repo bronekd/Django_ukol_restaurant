60445b1811ec97ea66e10998d721335d8c72a31c
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-07-15 20:54:15 +0200

    Skládání stránek

--- project_restaurant/app_restaurant/views.py
+++ project_restaurant/app_restaurant/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+"""def home(request):
+    return HttpResponse("Hello world!")
+"""
 def home(request):
-    template = loader.get_template('myfirst.html')
+    template = loader.get_template('home.html')
     return HttpResponse(template.render())
